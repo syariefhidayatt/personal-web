34c1dfdd88792c69a81829ff132c66a3871888b9
syariefhidayatt <syarif@Steady.localdomain>
2025-06-01 06:12:06 +0700

    update form

--- src/main.js
+++ src/main.js
@@ -5,3 +5,35 @@ const myTyped = new Typed("#greeting", {
     loop: true,
     typeSpeed: 70
 });
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    const response = await fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        Swal.fire({
+            icon: 'success',
+            title: 'Pesan Terkirim!',
+            text: 'Terimakasih.',
+            showConfirmButton: false,
+            timer: 2000
+        });
+        form.reset();
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Gagal mengirim pesan. Silakan coba lagi.'
+        });
+    }
+});
