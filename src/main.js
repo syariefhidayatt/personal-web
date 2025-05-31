0ee3325aa9449cbb2de721a9b7f8e08ffd69c3c2
syariefhidayatt <syarif@Steady.localdomain>
2025-06-01 01:47:50 +0700

    fix update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Typed from "typed.js";
 
 const myTyped = new Typed("#greeting", {
-    strings: ["Front-End Dev", "Back-End Dev", "Fullstack Dev"],
+    strings: ["Front-End Dev.", "Back-End Dev.", "Fullstack Dev."],
     loop: true,
     typeSpeed: 70
 });
